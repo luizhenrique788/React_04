d79aea3935dc08c67f7538c29019cc5b15c841cf
Luiz Henrique <luizhenriquesouza866@icloud.com>
2024-12-12 23:02:50 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ConversorDeMoedas from "./components/ConversorDeMoedas";
+
+function App() {
+  return (
+    <div className="App">
+      <ConversorDeMoedas />      
+    </div>
+  );
+}
+export default App;
